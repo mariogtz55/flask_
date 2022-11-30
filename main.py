a7fe875c775609401a996c77a72320133b661108
mariogtz55 <99363469+mariogtz55@users.noreply.github.com>
2022-11-30 16:50:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import os
 from conect import conection
 import time as time
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -11,6 +12,11 @@ tags=["id","timestamp", "vibracion"]
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route("/login")
+@cross_origin(supports_credentials=True)
+def login():
+  return jsonify({'success': 'ok'})
 
 @app.route('/getValues')
 def values():
