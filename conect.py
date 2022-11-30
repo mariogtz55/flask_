3548db58f5e26afa3bf7c7ad3fd45f61a3da5b0d
mariogtz55 <99363469+mariogtz55@users.noreply.github.com>
2022-11-30 10:47:27 -0600

    f

---
+++ conect.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+def conection():
+    host = "postgresciberfisico.postgres.database.azure.com"
+    dbname = "postgres"
+    user = "mariogtz5@postgresciberfisico"
+    password = "Ciberfisicos2022"
+    sslmode = "require"
+    
+    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+    conn = psycopg2.connect(conn_string)
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM cnc;")
+    rows = cursor.fetchall()
+    print("Read")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print('ready')
+    return rows[1]
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, jsonify
 import os
+from conect import conection
+import time as time
 
 app = Flask(__name__)
 
+
+tags=["id","timestamp", "vibracion"]
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/getValues')
+def values():
+    last=conection()
+    res = {tags[i]: last[i] for i in range(len(tags))}
+    jsonString = jsonify(res)
+    return jsonString
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
