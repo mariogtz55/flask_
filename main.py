53ca86a852c2730f4052a291d2b95b3add3bd00b
mariogtz55 <99363469+mariogtz55@users.noreply.github.com>
2022-11-30 17:23:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def index():
 def values():
     last=conection()
     res = {tags[i]: last[i] for i in range(len(tags))}
-    jsonString = jsonify(res)
-    response=jsonString.headers.add('Access-Control-Allow-Origin', '*')
-    return jsonString
+    response = jsonify(res)
+    response=response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
